816b06461881d7547161acaaaa0e37814e30a4b6
Hoomaan Sajjadi <hooman130@gmail.com>
2023-06-14 19:35:14 +0000

    ewf

--- main.py
+++ main.py
@@ -2,36 +2,67 @@ import re
 from flask import Flask, request, jsonify, render_template
 import openai
 import os
+from gunicorn.app.base import BaseApplication
+
+
+class FlaskApplication(BaseApplication):
+    def __init__(self, app, options=None):
+        self.options = options or {}
+        self.application = app
+        super().__init__()
+
+    def load_config(self):
+        config = {
+            key: value
+            for key, value in self.options.items()
+            if key in self.cfg.settings and value is not None
+        }
+        for key, value in config.items():
+            self.cfg.set(key.lower(), value)
+
+    def load(self):
+        return self.application
+
 
 app = Flask(__name__)
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-@app.route('/', methods=['GET'])
-def home():
-    return render_template('index.html')
 
-@app.route('/chat', methods=['POST'])
+@app.route("/", methods=["GET"])
+def home():
+    return render_template("index.html")
+
+
+@app.route("/chat", methods=["POST"])
 def chat():
     data = request.get_json()
-    user_message = data.get('message')
+    user_message = data.get("message")
 
     if not user_message:
-        return jsonify({'error': 'No message provided'}), 400
+        return jsonify({"error": "No message provided"}), 400
 
     try:
         response = openai.ChatCompletion.create(
-          model="gpt-4",  # Assuming GPT-4 is the model you're using
-          messages=[
+            model="gpt-4",  # Assuming GPT-4 is the model you're using
+            messages=[
                 {"role": "system", "content": "You are a helpful assistant."},
-                {"role": "user", "content": user_message}
-            ]
+                {"role": "user", "content": user_message},
+            ],
         )
-        return jsonify({'message': response['choices'][0]['message']['content']})
+        return jsonify({"message": response["choices"][0]["message"]["content"]})
     except Exception as e:
-        return jsonify({'error': str(e)}), 500
+        return jsonify({"error": str(e)}), 500
 
-if __name__ == '__main__':
 
-    app.run(debug=True)
-    
+if __name__ == "__main__":
+    # app.run(debug=True)
+    options = {
+        "bind": "0.0.0.0:8000",
+        "workers": 4,
+        "timeout": 120,
+        "graceful_timeout": 30,
+        "keepalive": 5,
+        "debug": True,
+    }
+    FlaskApplication(app, options).run()
