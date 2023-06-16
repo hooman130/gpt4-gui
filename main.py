d1dadb961b4262a212ea7ff0b42552e00fb7c368
Hoomaan Sajjadi <hooman130@gmail.com>
2023-06-16 04:52:51 +0000

    ewdfr

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
-from flask import Flask, request, jsonify, render_template, Response
-import openai
+"""
+This module contains a Flask application that uses OpenAI's GPT-4 model to chat with users.
+"""
+
 import os
-from gunicorn.app.base import BaseApplication
 import json
+from flask import Flask, request, jsonify, render_template, Response
+from gunicorn.app.base import BaseApplication
+import openai
 
 
 class FlaskApplication(BaseApplication):
+    """
+    A custom Gunicorn application that wraps the Flask application.
+    """
+
     def __init__(self, app, options=None):
+        """
+        Initializes the FlaskApplication object.
+
+        Args:
+            app (Flask): The Flask application object.
+            options (dict): A dictionary of Gunicorn options.
+        """
         self.options = options or {}
         self.application = app
         super().__init__()
 
     def load_config(self):
+        """
+        Loads the Gunicorn configuration.
+        """
         config = {
             key: value
             for key, value in self.options.items()
@@ -21,21 +39,36 @@ class FlaskApplication(BaseApplication):
             self.cfg.set(key.lower(), value)
 
     def load(self):
+        """
+        Loads the Flask application.
+        """
         return self.application
 
 
 app = Flask(__name__)
+"""
+The Flask application object.
+"""
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 @app.route("/", methods=["GET"])
 def home():
+    """
+    Renders the home page.
+    """
     return render_template("index.html")
 
 
 @app.route("/chat", methods=["POST"])
 def chat():
+    """
+    Handles chat requests from the user.
+
+    Returns:
+        Response: A Flask Response object containing the chat response.
+    """
     data = request.get_json()
     user_message = data.get("message")
 
@@ -43,6 +76,12 @@ def chat():
         return jsonify({"error": "No message provided"}), 400
 
     def generate():
+        """
+        Generates the chat response using OpenAI's GPT-4 model.
+
+        Yields:
+            str: A string containing the chat response.
+        """
         try:
             response = openai.ChatCompletion.create(
                 model="gpt-4-0613",  # Assuming GPT-4 is the model you're using
@@ -50,18 +89,18 @@ def chat():
                     {"role": "system", "content": "You are a helpful assistant."},
                     {"role": "user", "content": user_message},
                 ],
-                max_tokens=1024,
+                max_tokens=4096,
                 stream=True,
+                temprature=0.4,
             )
             # Format the response message
             for message in response:
-                # Format the response message
-                response_message = message["choices"][0]["message"]["content"]
+                response_message = message["choices"][0]["y"]
                 response_message = response_message.replace(". ", ".<br>")
                 yield f"data: {json.dumps({'message': response_message})}\n\n"
 
-        except Exception as e:
-            yield f"data: {json.dumps({'error': str(e)})}\n\n"
+        except Exception as error:
+            yield f"data: {json.dumps({'error': str(error)})}\n\n"
 
     return Response(generate(), mimetype="text/event-stream")
 
@@ -70,6 +109,7 @@ if __name__ == "__main__":
     app.run(
         debug=True,
     )
+
     """ options = {
         "bind": "0.0.0.0:8001",
         "workers": 4,
